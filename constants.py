7661e7f4cd1ec8e2d28f6e48623da572b8cde6a4
Ferrandeau Mathieu <m.ferrandeau@hotmail.fr>
2019-01-14 18:07:39 +0100

    Modification in constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 """Game constants"""
 
 MAP_HEIGHT = 15
-MAP_LENGTH = 15
+MAP_LENGHT = 15
 TILE_SIZE = 30
 
 UP = 0
--- main.py
+++ main.py
@@ -10,12 +10,9 @@ from constants import *
 def main():
 	"""Main Function"""
 	pygame.init()
-	window = pygame.display.set_mode((MAP_LENGHT * TILE_SIZE, MAP HEIGHT * TILE_SIZE))
+	window = pygame.display.set_mode((MAP_LENGHT * TILE_SIZE, MAP_HEIGHT * TILE_SIZE))
 	
-	"""config = load.config_from_file("config.json")
-				images= load.Images(config)
-				lvl = game.Lvl(load.map_from_file("maze.xsb"), config["items"])
-				mac_gyver =game.character"""
+	
 
 if __name__ == "__main__":
 	main()
